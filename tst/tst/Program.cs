f9ab38a59057fa318efa6a56e3e6dba3a80365c7
buildigor <buildigor@gmail.com>
2017-04-09 08:53:56 +0300

    test

---
+++ tst/tst/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiArray();
+            
+        }
+
+        private static void MultiArray()
+        {
+            byte[,]image = new byte[5,11];
+            for (int i = 0; i < 5; i++)
+            
+                for (int j = 0; j <= 10; j++)
+                
+                    image[i, j] = (byte) (i*j);
+                
+            
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                
+                    Console.Write(image[i,j]+"\t");
+                Console.WriteLine();
+                
+            }
+Console.ReadLine();
+        }
+        private static void FileWork()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                if (!File.Exists(@"d:\test1\" + i + ".txt"))
+                {
+                    File.Create(@"d:\test1\" + i + ".txt");
+                }
+            }
+            Thread.Sleep(2000);
+            for (int i = 0; i < 1000; i++)
+            {
+                if (File.Exists(@"d:\test1\" + i + ".txt"))
+                {
+                    File.Delete(@"d:\test1\" + i + ".txt");
+                }
+            }
+        }
+    }
+}
