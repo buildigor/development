0298267752c3fd65adcca00fc27b44cb8c5722d7
buildigor <buildigor@gmail.com>
2017-04-23 21:09:40 +0300

    Add Extension class

---
+++ Task1/Task1/Classes/Extension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Interfaces;
+
+namespace Task1.Classes
+{
+   public static class Extension
+    {
+        public static string ListCandyToString(this IEnumerable<Candy> list)
+        {
+            return list.Aggregate<ISweets, string>(null, (current, sweets) => current + (sweets.Name + "\n"));
+        }
+        public static string ListAllToString(this IEnumerable<ISweets> list)
+        {
+            return list.Aggregate<ISweets, string>(null, (current, sweets) => current + (sweets.Name + "\n"));
+        }
+    }
+}
--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -14,8 +14,28 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
-
+            LollipopCandy lollipop = new LollipopCandy("Dushes lollipopCandy", 2, 1.5, "circle", "green");
+            ChocolateCandy chocolateCandy = new ChocolateCandy("ChocolateCandy", 5, 3, "rectangular", 4);
+            StickOfConfectionery stickOfConfectioneryS = new StickOfConfectionery("Snikers stickOfConfectionery", 35, 27, 15);
+            StickOfConfectionery stickOfConfectioneryM = new StickOfConfectionery("Mars stickOfConfectionery", 25, 15, 20);
+            Wafer wafer = new Wafer("Chernomorskie wafers", 6, 3, 2);
+            NewYearsGift gift = new NewYearsGift
+            {
+                lollipop,chocolateCandy,stickOfConfectioneryM,stickOfConfectioneryS,wafer
+            };
+            Package package = new Package("green", gift);
+            Console.WriteLine("*********************Total Weight Candies******");
+            Console.WriteLine(gift.CandiesWeight);
+            Console.WriteLine("*********************Total Weight NewYearsGift******");
+            Console.WriteLine(gift.GiftWeight);
+            Console.WriteLine("*********************All Sweets*********************");
+            Console.WriteLine(gift.GetAllCandies().ListAllToString());
+            Console.WriteLine("*********************Finded Candyes******************");
+            Console.WriteLine(gift.FindCandies(1, 7).ListCandyToString());
+            Console.WriteLine("*********************Ordered Candyes*****************");
+            Console.WriteLine(gift.OrderBy(x => x.Name).ListCandyToString());
+            Console.WriteLine(gift.GetType().Name + " in package " + package.Color);
+            Console.ReadLine();
         }
     }
 }
