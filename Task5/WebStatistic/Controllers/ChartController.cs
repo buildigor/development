bbc202c4caec75aa10287b3f604bdeef3fd7a3e9
buildigor <buildigor@gmail.com>
2017-06-23 17:58:48 +0300

    Add Google Chart

---
+++ Task5/WebStatistic/Controllers/ChartController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BL;
+using WebStatistic.Models;
+
+namespace WebStatistic.Controllers
+{
+    public class ChartController : Controller
+    {
+        private IWorker _worker;
+
+        public ChartController()
+        {
+            _worker = new Worker();
+        }
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Chart()
+        {
+            return Json(Listdata(), JsonRequestBehavior.AllowGet);
+        }
+        public IEnumerable Listdata()
+        {
+            var managers = _worker.GetAllManagers();
+            List<ChartModel> dataForCharts = (from manager in managers let content = _worker.GetAllOrdersForManager(manager.Id) select new ChartModel() {ManagerName = manager.Name, SalesCount = content.Count()}).ToList();
+            var data = dataForCharts.AsEnumerable();
+            return data;
+        }
+    }
+}
---
+++ Task5/WebStatistic/Models/ChartModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStatistic.Models
+{
+    public class ChartModel
+    {
+        public string ManagerName { get; set; }
+        public int SalesCount { get; set; }
+    }
+}
